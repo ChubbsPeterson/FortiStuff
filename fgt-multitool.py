c2cf37cbbae8e052220fa01384190f8c26668b22
ChubbsPeterson <40532978+ChubbsPeterson@users.noreply.github.com>
2020-06-21 19:41:51 -0400

    Update fgt-multitool.py

--- fgt-multitool.py
+++ fgt-multitool.py
@@ -12,17 +12,6 @@ Run the script to display the help menu with command options
 """
 
 
-def sleepTimer(timer):
-    """
-    Countdown timer
-    """
-    for remaining in range(timer, 0, -1):
-        sys.stdout.write("\r")
-        sys.stdout.write("Please wait.. {:2d} seconds remaining..".format(remaining))
-        sys.stdout.flush()
-        time.sleep(1)
-
-
 def yes_or_no(question):
     """
     Yes or no question prompt
